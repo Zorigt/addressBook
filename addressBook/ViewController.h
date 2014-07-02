13dbd46528ff440d0c3fe278555637f3ff5e6870
Zorigt <Zorigt@gmail.com>
2014-07-02 11:31:54 -0700

    Initial Commit

---
+++ addressBook/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  addressBook
+//
+//  Created by Baz on 7/2/14.
+//  Copyright (c) 2014 Oscicoil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ addressBookTests/addressBookTests.m
@@ -0,0 +1,34 @@
+//
+//  addressBookTests.m
+//  addressBookTests
+//
+//  Created by Baz on 7/2/14.
+//  Copyright (c) 2014 Oscicoil. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface addressBookTests : XCTestCase
+
+@end
+
+@implementation addressBookTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
